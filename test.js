3324ba871ad7a5c363f053762f75450412c08dda
Robin Millette <robin@millette.info>
2016-04-07 04:10:20 -0400

    Tests pass; 100% coverage.

--- test.js
+++ test.js
@@ -5,14 +5,15 @@ import fn from './'
 
 test('no init', async t => {
   const result = await fn('yannicklux')
-  console.log('result:', result)
-  t.is(result, 'unicorns & rainbows')
+  // console.log('result:', result)
+  t.is(result.login, 'yannicklux')
 })
 
 test('init', async t => {
   fn.setLimiter(1, 15000)
   let result = await fn('yannicklux')
+  t.is(result.login, 'yannicklux')
   result = await fn('YannickKatambo')
-  console.log('result:', result)
-  t.is(result, 'unicorns & rainbows')
+  // console.log('result:', result)
+  t.is(result.login, 'YannickKatambo')
 })
