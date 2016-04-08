22db4a9be1fc2bd3f8adcce74dc54a756f739d78
Robin Millette <robin@millette.info>
2016-04-08 03:45:38 -0400

    Run tests in series (we're supposed to be rate limited after all).

--- test.js
+++ test.js
@@ -3,21 +3,22 @@
 import test from 'ava'
 import fn from './'
 
-test('no init', async t => {
+test.serial('no init', async t => {
   const result = await fn('yannicklux')
   t.is(result.login, 'yannicklux')
 })
 
-test('init', async t => {
-  fn.setLimiter(1, 30000)
+test.serial('init', async t => {
+  fn.setLimiter(1, 5000)
   let result = await fn('yannicklux')
   t.is(result.login, 'yannicklux')
+  fn.clearLimiter()
   result = await fn('YannickKatambo')
   t.is(result.login, 'YannickKatambo')
 })
 
-test('all', async t => {
-  fn.setLimiter(1, 60000)
+test.serial('all', async t => {
+  fn.setLimiter(1, 2500)
   const x = await Promise.all([fn('yannicklux'), fn('YannickKatambo')])
   t.is(x[0].login, 'yannicklux')
   t.is(x[1].login, 'YannickKatambo')
